d6a21e48837168247e2a295e77d15a1648fcdba3
hamzahbrown <habro4@student.monash.edu>
2015-08-28 11:48:58 +1000

    Please work

--- spiritLevelProcessor.js
+++ spiritLevelProcessor.js
@@ -39,82 +39,71 @@ freeze-button
 ****************************************************************************************/
 
 
+// these two lines make an array of zeros to a certain length (I don't understand it)
+//var pitchBuffer = Array.apply(null, Array(10)).map(Number.prototype.valueOf, 0);
+//var rollBuffer = Array.apply(null, Array(10)).map(Number.prototype.valueOf, 0);
+var pitchObject = {
+    buffer: [0, 0, 0, 0],
+    average: 0
+};
+var rollObject = {
+    buffer: [0, 0, 0, 0],
+    average: 0
+};
+
 function SpiritLevelProcessor() {
-    
-    // these two lines make an array of zeros to a certain length (I don't understand it)
-    var pitchBuffer = Array.apply(null, Array(25)).map(Number.prototype.valueOf, 0);
-    var rollBuffer = Array.apply(null, Array(25)).map(Number.prototype.valueOf, 0);
-    
+    "use strict";
     var self = this, uiController = null;
 
     self.initialise = function (controller) {
-    uiController = controller;
+        uiController = controller;
         
-    window.addEventListener("devicemotion", handleMotion);
+        // 'handleMotion' was used before it was defined'
+        window.addEventListener("devicemotion", handleMotion);
     };
-    
+
     function handleMotion(event) {
         var aX = event.accelerationIncludingGravity.x;
         var aY = event.accelerationIncludingGravity.y;
         var aZ = event.accelerationIncludingGravity.z;
-        
         var pitch = (Math.atan(-aY / aZ) * 180) / Math.PI;
         var roll = (Math.atan(aX / Math.sqrt(Math.pow(aY, 2) + Math.pow(aZ, 2))) * 180) / Math.PI;
         
-        rollBuffer = newBuffer(rollBuffer, roll);
-        pitchBuffer = newBuffer(pitchBuffer, pitch);
-        
-        var rollAverage = movingAverage(rollBuffer);
-        var pitchAverage = movingAverage(pitchBuffer);
-        
-        uiController.bubbleTranslate(rollAverage * 2, pitchAverage * 2, "dark-bubble");
-        
-        var target = document.getElementById("message-area");
-        
-        var xDisplay = Math.round(rollAverage);
-        var yDisplay = Math.round(-1*pitchAverage);
-        var zDisplay = 0; // what is the calculation here?
-        target.innerHTML = displayAngle(xDisplay, yDisplay, zDisplay);
-        
+        //console.log("pitch: " + pitch + " roll: " + roll)
+        //'movingAverage' was used before it was defined.	
+        pitchObject = movingAverage(pitchObject.buffer, pitch);
+        rollObject = movingAverage(pitchObject.buffer, roll);
+        uiController.bubbleTranslate(rollObject.average, pitchObject.average, "dark-bubble");
     }
 
-    function newBuffer(buffer, newValue) {
-        for (var i = 1; i<buffer.length; i++) {
+    function movingAverage(buffer, newValue) {
         var temp = buffer;
-
-        temp[i-1] = buffer[i];
+        for (var i = 0; i<buffer.length; i++){
+            temp[i+1] = buffer[i];
         }
-        temp[buffer.length-1] = newValue;
-        buffer = temp;
 
-        return buffer;
-    }
-    
-    function movingAverage(buffer) {
+        temp[0] = newValue;
+
+        buffer = temp.slice(0, buffer.length);
+
         var sum = 0;
         for (var i = 0; i<buffer.length; i++) {
+
             sum += buffer[i];
         }
         var average = sum / buffer.length;
-        return average;
+
+        return {buffer: buffer, average: average};
     }
 
     function displayAngle(x,y,z)
     {
-        /*var aX = event.accelerationIncludingGravity.x;
-        var aY = event.accelerationIncludingGravity.y;
-        var aZ = event.accelerationIncludingGravity.z;*/
-        
         // This function will handle the calculation of the angle from the z-axis and
         // display it on the screen inside a "div" tag with the id of "message-area"
 
         // Input: x,y,z
         //      These values should be the filtered values after the Moving Average for
         //      each of the axes respectively
-        //here is a comment
-
-        var displayString = "x: " + x + " y: " + y + " z: " + z;
-        return displayString;
     }
 
     self.freezeClick = function()
